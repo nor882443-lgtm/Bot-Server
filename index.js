8c613c116f2650cc615eea2ab055b95e2ad36eab
nor882443-lgtm <nor882443@gmail.com>
2026-09-12 08:25:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const serverHost = process.env.SERVER_HOST || 'DOOMS_DAY_REBORN.aternos.me';
 const serverPort = parseInt(process.env.SERVER_PORT || '59173', 10);
 const botUsername = process.env.BOT_USERNAME || '247_Monitor';
 const minecraftVersion = process.env.MC_VERSION || false;
-const reconnectInterval = parseInt(process.env.RECONNECT_INTERVAL_MS || '40000', 10);
-const antiAfkInterval = parseInt(process.env.ANTI_AFK_INTERVAL_MS || '20000', 10);
+const reconnectInterval = parseInt(process.env.servereco.aternos.me || '40000', 10);
+const antiAfkInterval = parseInt(process.env.56279 || '20000', 10);
 const httpPort = parseInt(process.env.PORT || '3000', 10);
 
 const app = express();
